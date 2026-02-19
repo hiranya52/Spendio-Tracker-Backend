80f184fe0e5ce1476376a3ce970b29dc007598e2
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-02-19 17:30:53 +0530

    Add User Entity class

--- src/main/java/edu/icet/model/entity/User.java
+++ src/main/java/edu/icet/model/entity/User.java
@@ -1,4 +1,27 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
 }
